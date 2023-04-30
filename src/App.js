fa82e13d66a463a835f11247494679dc7781cdec
salamaddin <77258564+salamaddin@users.noreply.github.com>
2023-04-30 05:02:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import "./styles.css";
+import React, { useState } from "react";
 
-export default function App() {
+const QuizGame = () => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Paris", "Rome", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "What is the currency of Japan?",
+      options: ["Dollar", "Euro", "Yen", "Pound"],
+      answer: "Yen",
+    },
+    {
+      question: "What is the highest mountain in the world?",
+      options: [
+        "Mount Kilimanjaro",
+        "Mount Everest",
+        "Mount Fuji",
+        "Mount Whitney",
+      ],
+      answer: "Mount Everest",
+    },
+  ];
+
+  const handleAnswer = (selectedOption) => {
+    if (selectedOption === questions[questionIndex].answer) {
+      setScore(score + 1);
+    }
+    setQuestionIndex(questionIndex + 1);
+  };
+
+  const currentQuestion = questions[questionIndex];
+
   return (
-    <div className="App">
-      <h1>Hello!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Quiz Game</h1>
+      {questionIndex < questions.length ? (
+        <div>
+          <h2>{currentQuestion.question}</h2>
+          {currentQuestion.options.map((option, index) => (
+            <button key={index} onClick={() => handleAnswer(option)}>
+              {option}
+            </button>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>Quiz finished</h2>
+          <p>Your score is {score}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default QuizGame;
